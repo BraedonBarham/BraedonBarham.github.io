245b17f1bb89da637058fc64a96b13f09712da3a
BraedonBarham <bbraedon10@gmail.com>
2025-04-07 00:28:38 -0700

    code4

--- HotelValidator/Program.cs
+++ HotelValidator/Program.cs
@@ -4,12 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Correctly written Hotels.xml File Results:");
         string xmlUrl = "https://braedonbarham.github.io/Hotels.xml";
         string xsdUrl = "https://braedonbarham.github.io/Hotels.xsd";
-        Console.WriteLine("Hi");
+        
 
         string result = XmlUtilities.Verification(xmlUrl, xsdUrl);
         Console.WriteLine(result);
         Console.ReadKey();
+
+        Console.WriteLine("Incorrectly written Hotels.xml File Results:");
+        string xmlUrl = "https://braedonbarham.github.io/HotelsErrors.xml";
+        string result = XmlUtilities.Verification(xmlUrl, xsdUrl);
+        Console.WriteLine(result);
+        Console.ReadKey();
+
     }
 }
